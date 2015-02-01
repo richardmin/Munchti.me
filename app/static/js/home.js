bc51fd0a1ad59ef43b54034a9fc780a836b3fd8e
Richard Min <richardmin97@g.ucla.edu>
2015-02-01 14:53:29 -0800

    Final commit (@SBHacks)- resolved the issues with POST
    Good job guys! - todo: error form for the form aren't working, smooth out animations, and add more detailed GET on requests

--- app/static/js/home.js
+++ app/static/js/home.js
@@ -12,22 +12,15 @@ function loadOrders(){
 $('#orderForm').submit(function(event) {
     event.preventDefault();  
 
-    function userFormError() {
-        $('#orderError').html('<center>You have form errors. Please go back and ensure mandatory forms are filled.</center>');
+    function userOrderError() {
+        $('#orderError').html(error('<center>We couldn\'t save your order. Please refresh and try again.</center>'));
         return;
     }
-
-
-	function userOrderError() {
-		$('#orderError').html(error('<center>We couldn\'t save your order. Please refresh and try again.</center><br />'));
-		return;
-	}
     function userOrderSuccess() {
         $('#orderError').html('<div data-alert class="alert-box success radius"><strong>We have recieved your order! Please refresh to submit a new one.</strong></div>');
         return;
     }
     var data = $('#orderForm').serializeObject();
-<<<<<<< HEAD
     console.log(JSON.stringify(data));
     $.ajax({
         type: 'POST',
@@ -44,33 +37,3 @@ $('#orderForm').submit(function(event) {
 });
 
 
-=======
-    if(data.name=="" || data.food=="" || data.destination==""|| data.time=="" || data.payment=="" || data.contact=="" )
-    {
-        userFormError();
-    }
-    else
-    {
-        $.ajax({
-            type: 'POST',
-            url: '/api/post',
-            data: JSON.stringify(data),
-            contentType: 'application/json',
-        }).done(function(order, textStatus, jqXHR) {
-            $("#submit").hide();
-            userOrderSuccess();
-        }).fail(function(error, textStatus, jqXHR) {
-            userOrderError();
-        });
-    }
-});
-
-$('')
-
-// $(function() {
-//     $('#orderForm').submit(function() {
-//         $('#result').text(JSON.stringify($('#orderForm').serializeObject()));
-//         return false;
-//     });
-// });
->>>>>>> b6b1396e55f93527bf6d63c654f743306c149fcb
